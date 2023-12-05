3e2a18a75281fc0d7c84ad44e91770282b7918f7
Noah Alemayehu <noahlem21@gmail.com>
2023-12-05 03:55:03 -0800

    test_save_reload_base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ Initialize the BaseModel class """
 
+        from models import storage
         if not kwargs:
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -29,7 +31,10 @@ class BaseModel:
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime"""
+
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__
---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" A class that serializes and deserializes json
+    file to instances
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """ A class that serializes and deserializes json
+    file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects """
+
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id """
+
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+
+        with open(self.__file_path, "w") as f:
+            dic_storage = {}
+            for key, val in self.__objects.items():
+                dic_storage[key] = val.to_dict()
+            json.dump(dic_storage, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists
+        """
+
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                for obj in json.load(f).values():
+                    self.new(eval(obj["__class__"])(**obj))
+        except FileNotFoundError:
+            pass
